46280077426a173cf6be8d6201d9e97765e2cae0
Nick Reuter <nreuter85@gmail.com>
2016-01-07 22:44:00 -0600

    gt58-common: clean up and inherit from msm8916-common
    Change-Id: I758bc6e689405c05c30b8c17ca3f6b5f3ac0504a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,34 +20,10 @@
 # definition file).
 #
 
-# inherit from qcom-common
--include device/samsung/qcom-common/BoardConfigCommon.mk
+# inherit from msm8916-common
+-include device/samsung/msm8916-common/BoardConfigCommon.mk
 
 LOCAL_PATH := device/samsung/gt58-common
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8916
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
-TARGET_BOOTLOADER_BOARD_NAME := MSM8916
-
-# Arch
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_CPU_VARIANT := cortex-a53
-TARGET_CPU_CORTEX_A53 := true
-
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/gt58-common/include
-
-# Kernel
-TARGET_KERNEL_ARCH := arm
-BOARD_DTBTOOL_ARG := -2
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=enforcing
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_RAMDISK_OFFSET := 0x02000000
-BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_SOURCE := kernel/samsung/msm8916
 
 # Partition sizes
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -59,106 +35,15 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/gt58-common/bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
 
-# Custom RIL class
-BOARD_RIL_CLASS := ../../../device/samsung/gt58-common/ril/
-
-# NFC
-BOARD_HAVE_NFC := true
-
-# GPS
-TARGET_GPS_HAL_PATH := device/samsung/gt58-common/gps
-BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
-
-# Fonts
-EXTENDED_FONT_FOOTPRINT := true
-
-# malloc implementation
-MALLOC_IMPL := dlmalloc
-
-# Audio
-BOARD_USES_ALSA_AUDIO := true
-AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
-
-# Charger
-BOARD_CHARGER_SHOW_PERCENTAGE := true
-BOARD_CHARGER_ENABLE_SUSPEND := true
-
-# Enable QCOM FM feature
-TARGET_QCOM_NO_FM_FIRMWARE := true
-
-# Enable HW based full disk encryption
-TARGET_HW_DISK_ENCRYPTION := true
-
-# Build our own PowerHAL
-#TARGET_POWERHAL_SET_INTERACTIVE_EXT := $(LOCAL_PATH)/power/power_ext.c
-TARGET_POWERHAL_VARIANT := qcom
-
-# Wifi
-BOARD_HAS_QCOM_WLAN 		 := true
-BOARD_HAS_QCOM_WLAN_SDK 	 := true
-BOARD_HOSTAPD_DRIVER 		 := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB 	 := lib_driver_cmd_qcwcn
-BOARD_WLAN_DEVICE 		 := qcwcn
-BOARD_WPA_SUPPLICANT_DRIVER 	 := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
-TARGET_PROVIDES_WCNSS_QMI        := true
-TARGET_USES_QCOM_WCNSS_QMI 	 := true
-TARGET_USES_WCNSS_CTRL 		 := true
-WIFI_DRIVER_FW_PATH_AP 		 := "ap"
-WIFI_DRIVER_FW_PATH_STA 	 := "sta"
-WPA_SUPPLICANT_VERSION 		 := VER_0_8_X
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
-
-# Vold
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
-BOARD_VOLD_MAX_PARTITIONS := 65
-
-# Camera
-TARGET_PROVIDES_CAMERA_HAL := true
-USE_DEVICE_SPECIFIC_CAMERA := true
-COMMON_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
-
-# CMHW
-BOARD_HARDWARE_CLASS += device/samsung/gt58-common/cmhw
-
-# Workaround to avoid issues with legacy liblights on QCOM platforms
-TARGET_PROVIDES_LIBLIGHT := true
-
-# Qcom
-BOARD_USES_QC_TIME_SERVICES := true
-TARGET_USES_QCOM_BSP := true
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
-PROTOBUF_SUPPORTED := true
-HAVE_SYNAPTICS_I2C_RMI4_FW_UPGRADE := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
-
-# Media
-TARGET_QCOM_MEDIA_VARIANT := caf
-
-# Display
-TARGET_CONTINUOUS_SPLASH_ENABLED := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-
-# ANT+
-BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
-
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/gt58-common/sepolicy
+# Dex
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),user)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
 
 # Misc.
 TARGET_SYSTEM_PROP := device/samsung/gt58-common/system.prop
@@ -177,6 +62,12 @@ TW_TARGET_USES_QCOM_BSP := true
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/gt58-common/recovery/recovery_keys.c
 BOARD_HAS_NO_SELECT_BUTTON := true
 
+# Misc.
+BOARD_RECOVERY_SWIPE := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_NO_REBOOT_BOOTLOADER := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
 # Storage
 TARGET_RECOVERY_FSTAB := device/samsung/gt58-common/rootdir/etc/fstab.qcom
 TW_INTERNAL_STORAGE_PATH := "/data/media"
@@ -187,17 +78,7 @@ TW_NO_USB_STORAGE := true
 TW_MTP_DEVICE := /dev/usb_mtp_gadget
 RECOVERY_SDCARD_ON_DATA := true
 
-# Misc.
-BOARD_RECOVERY_SWIPE := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_NO_REBOOT_BOOTLOADER := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-
-# Dex
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+BOARD_VOLD_MAX_PARTITIONS := 65
